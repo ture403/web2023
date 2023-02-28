26b1f9088801f099de68c9afc4fae0f71d6d3e38
ture403 <93423185+ture403@users.noreply.github.com>
2023-02-28 19:41:43 +0900

    23.02.28
    오늘 끝

--- even/even.js
+++
@@ -1,6 +0,0 @@
-let userNumber = prompt("숫자를 입력하세요.");
-
-if (userNumber !== null){
-    userNumber = parseInt(userNumber);
-    (userNumber %2 === 0) ? alert(`${userNumber} : 짝수`) : alert(`${userNumber} : 홀수`);
-}
