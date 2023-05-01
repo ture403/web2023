250514c16cadae0ed232486a884cedcea74e9e2c
ture403 <ture403@gmail.com>
2023-05-01 18:03:27 +0900

    2023-05-01

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -66,6 +66,11 @@ const musicProgress = musicWrap.querySelector(".progress");
 const musicProgressBar = musicWrap.querySelector(".progress .bar");
 const musicProgressCurrent = musicWrap.querySelector(".progress .timer .current");
 const musicProgressDuration = musicWrap.querySelector(".progress .timer .duration");
+const musicRepeat = musicWrap.querySelector("#control-repreat");
+const musicListBtn = musicWrap.querySelector("#control-list");
+const musicList  = musicWrap.querySelector(".music_list");
+const musicListUl  = musicWrap.querySelector(".music_list ul");
+const musicListClose  = musicWrap.querySelector(".music_list h3 .close");
 
 
 let musicIndex = 1; //현재 음악 인덱스
@@ -103,6 +108,7 @@ const prevMusic = () => {
     musicIndex == 1 ? musicIndex = allMusic.length : musicIndex--;
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();
 }
 
 //다음 곡 듣기
@@ -113,6 +119,7 @@ const nextMusic = () => {
     musicIndex == allMusic.length ? musicIndex = 1 : musicIndex++;
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();
 }
 
 // 뮤직 진행바
@@ -125,7 +132,7 @@ musicAudio.addEventListener("timeupdate", e => {
     //전체 시간
     musicAudio.addEventListener("loadeddata",()=>{
         let audiodrutaion = musicAudio.duration;
-        console.log(audiodrutaion)
+        // console.log(audiodrutaion)
         let totalMin = Math.floor(audiodrutaion / 60);
         let totalSec = Math.floor(audiodrutaion % 60);
         if(totalSec < 10) totalSec = `0${totalSec}`;
@@ -149,6 +156,54 @@ musicProgress.addEventListener("click", (e)=> {
     //백분위로 나눈 숫자에 다시 전체 길이를 곱해서 현재 재생값으로 바꿈
     musicAudio.currentTime = (clickedOffsetX/progressWidth) *songDuration;
 })
+//반복 버튼 클릭
+musicRepeat.addEventListener("click",()=>{
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat" :
+        musicRepeat.setAttribute("class", "repeat_one");
+        musicRepeat.setAttribute("title", "한곡 반복");
+        break;
+        case "repeat_one" :
+        musicRepeat.setAttribute("class", "shuffle");
+        musicRepeat.setAttribute("title", "랜덤 반복");
+        break;
+        case "shuffle" :
+            musicRepeat.setAttribute("class", "repeat");
+            musicRepeat.setAttribute("title", "전체 반복");
+        break;
+    }
+})
+//오디오 끝나면
+musicAudio.addEventListener("ended", ()=>{
+    let getAttr = musicRepeat.getAttribute("class");
+
+    switch(getAttr){
+        case "repeat" :
+            nextMusic();
+        break;
+        case "repeat_one":
+            playMusic();
+        break;
+        case "shuffle" :
+            //랜덤인덱스 생성
+            let randomIndex = Math.floor(Math.random() * allMusic.length +1);
+
+            do {
+                randomIndex = Math.floor(Math.random() * allMusic.length +1);
+            }while (musicIndex == randomIndex);
+
+
+            //현재 인덱스를 댄덤 인텍스로 변경
+            musicIndex = randomIndex; 
+            
+            loadMusic(randomIndex);
+            playMusic();
+        break;
+    }
+    playListMusic();
+});
 
 //플레이버튼 클릭
 musicplay.addEventListener("click",()=>{
@@ -166,6 +221,98 @@ musicNextBtn.addEventListener("click",()=>{
     nextMusic();
 });
 
+//뮤직 리스트 버튼
+musicListBtn.addEventListener("click",()=>{
+    musicList.classList.add("show");
+})
+
+//뮤직 리스트 닫기
+musicListClose.addEventListener("click",()=>{
+    musicList.classList.remove("show");
+});
+
+//뮤직 리스트 규현하기
+for(let i=0; i<allMusic.length; i++){
+    let li = `
+        <li data-index="${i+1}">
+        <span class="img">
+            <img src="img/${allMusic[i].img}.png" alt=${allMusic[i].name}>
+        </span>
+        <div class="title">
+            <strong>${allMusic[i].name}</strong>
+            <em>${allMusic[i].artist}</em>
+            <audio class ="${allMusic[i].audio}" src="audio/${allMusic[i].audio}.mp3"></audio>
+        </div>
+        <span class="audio-duration" id="${allMusic[i].audio}">3:04</span>
+        </li> 
+    `;
+    // musicListUl.innerHTML += li;
+    musicListUl.insertAdjacentHTML("beforeend",li);
+
+    //리스트에서 시간을 표시할 선택자 
+    let liAudioDuration = musicListUl.querySelector(`#${allMusic[i].audio}`);
+
+    //리스트에서 오디오 파일 가져오기
+    let liAudio = musicList.querySelector(`.${allMusic[i].audio}`);
+    liAudio.addEventListener("loadeddata",()=>{
+        let audiodrutaion = liAudio.duration;
+        let totalMIn = Math.floor(audiodrutaion / 60);
+        let totalSec = Math.floor(audiodrutaion % 60);
+        if(totalSec <10 ) totalSec =`0${totalSec}`;
+        liAudioDuration.innerText =`${totalMIn}:${totalSec}`;
+        liAudioDuration.setAttribute("data-duration",`${totalMIn}:${totalSec}`);
+    });
+}
+// 뮤직 리스트를 클릭하면 재생
+function playListMusic(){
+    const musicListAll = musicListUl.querySelectorAll("li");    //뮤직 리스트 목록
+    for(let i=0; i<musicListAll.length; i++){
+        let audioTag = musicListAll[i].querySelector(".audio-duration");
+        musicListAll[i].setAttribute("onclick", "clicked(this)");
+
+        if(musicListAll[i].classList.contains("playing")){
+            musicListAll[i].classList.remove("playing");
+            let dataAudioDuration = audioTag.getAttribute("data-duration");
+            audioTag.innerText= dataAudioDuration;
+        }
+        if(musicListAll[i].getAttribute("data-index") == musicIndex){
+            musicListAll[i].classList.add("playing");
+            audioTag.innerText="재생중";
+        }
+    }
+}
+
+playListMusic();
+
+// 뮤직 리스크를 클릭하면
+function clicked(el){
+    let getIndex = el.getAttribute("data-index");
+    // alert(getIndex);
+    musicIndex = getIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playListMusic();
+}
+
+// 그런데 load 이벤트는 문서내의 모든 리소스(이미지, 스크립트)의
+// 다운로드가 끝난 후에 실행된다. 이것을 에플리케이션의 구동이
+// 너무 지연되는 부작용을 초래할 수 있다.
+window.addEventListener("DOMContentLoaded", () => {
+    loadMusic(musicIndex);
+});
+// DOMContentLoaded는 문서에서 스크립트 작업을 할 수 있을 때
+// 실행되기 때문에 이미지 다운로드를 기다릴 필요가 없다.
+// window.addEventListener('DOMContentLoaded', function(){
+//     console.log('DOMContentLoaded');
+// })
+
+
+
+
+
+
+
 window.addEventListener("load",()=>{
     loadMusic(musicIndex);
+    playListMusic();
 });
